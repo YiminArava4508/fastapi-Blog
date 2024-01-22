2ba59b3328423f620b628a285d064cc8fb86b61c
Yimin Arava <85930454+YiminArava4508@users.noreply.github.com>
2024-01-22 15:29:12 -0700

    Create test_post.py

---
+++ tests/test_post.py
@@ -0,0 +1,19 @@
+import pytest
+from app import schemas
+
+
+def test_get_all_posts(authorized_client, test_posts):
+    res = authorized_client.get("/posts/")
+
+    def validate(post):
+        return schemas.PostOut(**post)
+    posts_map = map(validate, res.json())
+    posts_list = list(posts_map)
+
+    assert len(res.json()) == len(test_posts)
+    assert res.status_code == 200
+
+
+def test_get_one_post_not_exist(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/88888")
+    assert res.status_code == 404
